3ba8bd2de7ab1c81f8c37bb305d17f23bca14fcb
Michael Wu <mwu@mozilla.com>
2014-02-12 19:05:27 -0500

    Add falcon support

--- config.sh
+++ config.sh
@@ -30,8 +30,8 @@ case `uname` in
 	exit -1
 esac
 
-GITREPO=${GITREPO:-"git://github.com/mozilla-b2g/b2g-manifest"}
-BRANCH=${BRANCH:-master}
+GITREPO=${GITREPO:-"git://github.com/michaelwu/b2g-manifest"}
+BRANCH=${BRANCH:-kk_3.5_rb1.11-falcon}
 
 while [ $# -ge 1 ]; do
 	case $1 in
@@ -110,7 +110,7 @@ case "$1" in
 	repo_sync $1
 	;;
 
-"otoro"|"unagi"|"keon"|"inari"|"leo"|"hamachi"|"peak"|"helix"|"wasabi")
+"otoro"|"unagi"|"keon"|"inari"|"leo"|"hamachi"|"peak"|"helix"|"wasabi"|"falcon")
 	echo DEVICE=$1 >> .tmp-config &&
 	repo_sync $1
 	;;
